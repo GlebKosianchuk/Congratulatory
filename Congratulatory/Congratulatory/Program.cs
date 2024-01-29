5ccb233e416a5e0b5ed8b59ef85e2e6318d89e2e
Gleb Kasianchuk <glebkosianchuk@mail.ru>
2024-01-29 17:51:09 +0300

    Test table population

--- Congratulatory/Congratulatory/Program.cs
+++ Congratulatory/Congratulatory/Program.cs
@@ -12,5 +12,22 @@ public class Program
     public static void Main(string[] args)
     {
         using var context = new CongratulatoryDbContext();
+        var person1 = new Person()
+        {
+            Name = "Michael",
+            Surname = "Ballack",
+            Date = new DateOnly(1974, 7, 12)
+        };
+
+        var person2 = new Person()
+        {
+            Name = "John",
+            Surname = "Stones",
+            Date = new DateOnly(1994, 4, 16)
+        };
+
+        context.Persons.Add(person1);
+        context.Persons.Add(person2);
+        context.SaveChanges();
     }
 }
