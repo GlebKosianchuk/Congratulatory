da3e56d6092901351d203b6f8c41f4cd0d172623
Gleb Kasianchuk <glebkosianchuk@mail.ru>
2024-01-29 16:31:17 +0300

    Set up Person mapping

---
+++ Congratulatory/Congratulatory.Db/CongratulatoryDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Congratulatory.Db;
+
+public class CongratulatoryDbContext : DbContext
+{
+    public CongratulatoryDbContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Person> Persons { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("");
+    }
+}
--- Congratulatory/Congratulatory/Program.cs
+++ Congratulatory/Congratulatory/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Congratulatory.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Congratulatory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var context = new CongratulatoryDbContext();
+    }
+}
